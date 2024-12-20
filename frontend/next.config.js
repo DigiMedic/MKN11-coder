e540a88e0d7f1973471c24e23346a373a01d89d0
petrsovadina <petr.sovadina9@gmail.com>
2024-12-20 12:05:15 +0100

    ok

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        },
-      ],
-    }
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` : 'http://backend:8000/api/:path*',
+      },
+    ]
   },
 }
 
