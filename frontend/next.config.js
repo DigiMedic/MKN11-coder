6e8da74ed784ba24b22ea9bd6ca7defb5c9390fd
petrsovadina <petr.sovadina9@gmail.com>
2024-12-20 11:56:11 +0100

    ok

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        },
+      ],
+    }
   },
 }
 
